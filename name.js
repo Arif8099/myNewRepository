101eb1f4f526041ebb1fc8aaa559ef2e931f778f
arif <mullaarif43@gmail.com>
2018-12-29 23:47:09 +0530

    Fixed error in javaScript file

--- name.js
+++ name.js
@@ -1,3 +1,10 @@
 let name = prompt("What is your name?");
 
 console.log("Your name is " + name);
+
+function greet(name){
+	console.log("Hello " + name +"!");
+}
+
+let name = "Arif";
+greet(name);
